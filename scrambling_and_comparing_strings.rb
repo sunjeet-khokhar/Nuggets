5397aec241db04b9c9d2fbf493bdb2ab464722e9
sunjeet-khokhar <sunjeet81@gmail.com>
2016-10-06 11:51:08 +1300

    string srambler nuggest

---
+++ scrambling_and_comparing_strings.rb
@@ -0,0 +1,33 @@
+=begin
+Write function scramble(str1,str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+For example:
+str1 is 'rkqodlw' and str2 is 'world' the output should return true.
+str1 is 'cedewaraaossoqqyt' and str2 is 'codewars' should return true.
+str1 is 'katas' and str2 is 'steak' should return false.
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered
+=end
+
+def scrambles(s1,s2)
+arr1 = s1.split('')
+arr2 = s2.split('')
+t =0
+f =0
+arr2.each { |x| if arr1.include?(x)
+t = t+1
+else
+f = f+1
+end
+}
+if t == arr2.length
+  print true
+else
+  print false
+end
+
+end
+
+
+scrambles('cedewaraaooqqyt','codewars')
